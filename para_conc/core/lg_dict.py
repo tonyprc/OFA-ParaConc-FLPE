3e822ffcacdb20602666d35fc4caba07cae82b68
tonyprc <47979580+tonyprc@users.noreply.github.com>
2024-10-18 17:12:40 +0800

    Create lg_dict.py

---
+++ para_conc/core/lg_dict.py
@@ -0,0 +1,12 @@
+LG_DICT={
+    "0":"put your valid kimi secret key here",
+    "1":"put your valid kimi secret key here",
+    "2":"put your valid kimi secret key here", 
+    "3":"put your valid kimi secret key here",
+    "4":"put your valid kimi secret key here",
+    "5":"put your valid kimi secret key here", 
+    "6":"put your valid kimi secret key here",
+    "7":"put your valid kimi secret key here",
+    "8":"put your valid kimi secret key here", 
+    "9":"put your valid kimi secret key here"
+    }
